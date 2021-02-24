0a19d36b388bfbab7bf0c17d262857192482ec80
Boganski <nicolas.boganski@epita.fr>
2021-02-24 22:39:01 +0200

    utf 8 problems solved

--- Crawler/Crawler/main.cpp
+++ Crawler/Crawler/main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
-//#include "curl/curl.h"
+#include "curl/curl.h"
 //#include <libxml/HTMLparser.h>
 //#include <libxml/xpath.h>
 //#include <libxml/uri.h>
 #include "Keyworde.h"
 #include <string>
+#include <locale> 
+#include <codecvt>
+#include <fstream>
+
 
 using namespace std;
+
+static size_t cb(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+
 int main(int argc, char* argv[])
 {
-    /*
     CURL* req = curl_easy_init();
     CURLcode res;
+    string wow;
     if (req)
     {
-        curl_easy_setopt(req, CURLOPT_URL, "www.google.com");
-        cout << "wow1" << endl;
-        curl_easy_setopt(req, CURLOPT_FOLLOWLOCATION, 1L);
-        cout << "wow2" << endl;
+        curl_easy_setopt(req, CURLOPT_URL, "https://lahorde.samizdat.net");
+        //curl_easy_setopt(req, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, cb);
+        curl_easy_setopt(req, CURLOPT_WRITEDATA,&wow);
         res = curl_easy_perform(req);
-        cout << "wow3" << endl;
         if (res != CURLE_OK)
         {
             fprintf(stderr, "curl_easy_operation() failed : %s\n", curl_easy_strerror(res));
         }
     }
     curl_easy_cleanup(req);
+    wstring wwow = wstring_convert<codecvt_utf8<wchar_t>>().from_bytes(wow);
+    //cout << wow << endl << wow.size();
+
+    wofstream fout("example.txt"); 
+    locale loc(locale::classic(), new codecvt_utf8<wchar_t>);
+    fout << wwow << endl;
+    //wcout << wwow << endl << wwow.size();
     return 0;
-    */
+    //*/
+    /*
     Keyworde a = Keyworde();
     string ee = "wouf";
     string ff = "woel";
@@ -36,6 +55,6 @@ int main(int argc, char* argv[])
     a + &ff;
     a + &gg;
     a.printkey();
-    return 0;
+    return 0; //*/
 
 }
