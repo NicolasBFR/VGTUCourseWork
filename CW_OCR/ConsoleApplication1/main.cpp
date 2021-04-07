8f72385590f04ec1f6735dd2331754fb92e047af
Boganski <nicolas.boganski@epita.fr>
2021-04-07 10:09:58 +0300

    All classes defined

--- CW_OCR/ConsoleApplication1/main.cpp
+++ CW_OCR/ConsoleApplication1/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <SDL.h>
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	cout << sizeof(bool); 
+	string tmp;
+	getline(cin, tmp);
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
